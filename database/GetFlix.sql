2af913279625b6373001a8ad091622c41b9042cf
Lord of Chicken <77158261+Lord-of-Chicken@users.noreply.github.com>
2021-04-12 10:04:52 +0200

    commit 10:04

---
+++ database/GetFlix.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : database:3306
+-- Généré le :  Dim 11 avr. 2021 à 20:26
+-- Version du serveur :  10.4.2-MariaDB-1:10.4.2+maria~bionic
+-- Version de PHP :  7.2.5
+CREATE DATABASE IF NOT EXISTS GetFlix;
+USE GetFlix;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+01:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `GetFlix`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `commentID` int(50) NOT NULL,
+  `comment` text NOT NULL,
+  `parent_id` int(50) DEFAULT NULL,
+  `moovieid` int(10) DEFAULT NULL,
+  `userid` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`commentID`, `comment`, `parent_id`, `moovieid`, `userid`) VALUES
+(10, 'jkdfskjjkdsjkf', NULL, NULL, '33'),
+(11, 'bloups', NULL, NULL, '33'),
+(12, 'dmllmdfslms', NULL, NULL, '33'),
+(13, 'qeDFSFDVD', NULL, NULL, '33'),
+(14, 'QDGSFD', NULL, 399566, '33'),
+(15, 'blip', NULL, NULL, '33');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`commentID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
