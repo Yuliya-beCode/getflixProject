3c6a9d7d73220e90f037bbefd8be2b40d821ffc0
RobbertKlockaerts <robbert.klockaerts@gmail.comm>
2021-04-12 14:40:19 +0200

    404

---
+++ application/source/404.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404-page</title>
+    <link rel="stylesheet" href="404.css" type="text/css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+          crossorigin="anonymous"/>
+
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <img id="logo" src="pictures/Logo.png" alt="logo" srcset="" width="75px">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Welcome Page</a>
+                    </li>
+                <?php if (isset($_SESSION['auth'])) : ?>
+              
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="logout.php">Log Out</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="account.php">Profils</a>
+                    </li>
+               
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="admin.php">Admin</a>
+                    </li>
+               
+                    <?php else : ?>
+                        <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="register.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="login.php">Log in</a>
+                    </li>
+                    
+                 
+
+                    <?php endif; ?>
+
+                </ul>
+            </div>
+        </div>
+        <header class="container">
+    <div class="d-flex justify-content-end">
+        <div id="myform" class="searchbar">
+            <label class="search_label" for="search_input">Search :</label>
+            <input id="search_input" class="search_input" type="text" name="search" onkeypress="clickPress(event)">
+            <button id="btn_search" aria-label="search" class="search_icon"><i class="fas fa-search"></i></button>
+        </div>
+    </div>
+</header>
+    </nav>
+</header>
+
+<div class="container text-center align-middle text-white place-above">
+    <h1>Hey you!</h1>
+    <h3>This page doesn't exist!<h3>  
+    <h3>Sorry 404-page!</h3><a href="index.php" class="btn btn-secondary btn-me active mt-1" role="button" aria-pressed="true">Return</a>
+</div>
+
+</html>
